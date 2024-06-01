412839b1cec2f58b09eeb59c31bb62580129c380
Yama-28 <bluestrobe.48@gmail.com>
2024-06-01 17:21:21 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [3] = LAYOUT_universal(
-    LAY_TAG,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                           KC_NO,          KC_NO,   KC_NO,    KC_NO,    KC_NO,          KC_NO,
+    LAY_TOG,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                           KC_NO,          KC_NO,   KC_NO,    KC_NO,    KC_NO,          KC_NO,
     KC_TRANSPARENT, KC_NO,          KC_NO,          KC_NO,          KC_NO,          RGB_TOG,                                         QK_KB_0,        QK_KB_1, QK_KB_13, QK_KB_14, QK_KB_15,       KC_NO,
     KC_TRANSPARENT, KC_NO,          RGB_SAI,        RGB_HUI,        RGB_VAI,        RGB_MODE_FORWARD,                                QK_KB_11,       QK_KB_1, QK_KB_8,  QK_KB_11, KC_NO,          KC_NO,
     KC_TRANSPARENT, KC_NO,          RGB_SAD,        RGB_HUD,        RGB_VAD,        RGB_MODE_REVERSE, KC_TRANSPARENT, CS(KC_ESCAPE), QK_KB_12,       QK_KB_3, QK_KB_9,  QK_KB_12, KC_NO,          KC_NO,
