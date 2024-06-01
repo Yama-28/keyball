9d735eea13077ae60fd074e3a5892aa00354a890
Yama-28 <bluestrobe.48@gmail.com>
2024-06-01 17:25:50 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -31,11 +31,11 @@ enum my_keyball_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
-    KC_ESCAPE,     KC_1,        KC_2,        KC_3,        KC_4,                KC_5,                                                    KC_6,         KC_7,    KC_8,     KC_9,     KC_0,         KC_BSPC,
-    KC_TAB,        KC_Q,        KC_W,        KC_E,        KC_R,                KC_T,                                                    KC_Y,         KC_U,    KC_I,     KC_O,     KC_P,         KC_LEFT_BRACKET,
-    KC_LEFT_CTRL,  KC_A,        KC_S,        KC_D,        KC_F,                KC_G,                                                    KC_H,         KC_J,    KC_K,     KC_L,     KC_SEMICOLON, KC_QUOTE,
-    KC_LEFT_SHIFT, KC_Z,        KC_X,        KC_C,        KC_V,                KC_B,           KC_ESCAPE,           KC_PRINT_SCREEN,    KC_N,         KC_M,    KC_COMMA, KC_DOT,   KC_SLASH,     KC_INTERNATIONAL_1,
-    KC_LEFT_ALT,   KC_LEFT_GUI, KC_LEFT_CTL, KC_LEFT_ALT, LT(1,KC_LANGUAGE_2), LT(2,KC_SPACE), LT(3,KC_LANGUAGE_1), LT(2,KC_BACKSPACE), LT(1,KC_ENT), _______, _______,  _______,  KC_RALT,      QK_KB_10
+    KC_ESCAPE,     KC_1,        KC_2,         KC_3,        KC_4,                KC_5,                                                    KC_6,         KC_7,    KC_8,     KC_9,     KC_0,         KC_BSPC,
+    KC_TAB,        KC_Q,        KC_W,         KC_E,        KC_R,                KC_T,                                                    KC_Y,         KC_U,    KC_I,     KC_O,     KC_P,         KC_LEFT_BRACKET,
+    KC_LEFT_CTRL,  KC_A,        KC_S,         KC_D,        KC_F,                KC_G,                                                    KC_H,         KC_J,    KC_K,     KC_L,     KC_SEMICOLON, KC_QUOTE,
+    KC_LEFT_SHIFT, KC_Z,        KC_X,         KC_C,        KC_V,                KC_B,           KC_ESCAPE,           KC_PRINT_SCREEN,    KC_N,         KC_M,    KC_COMMA, KC_DOT,   KC_SLASH,     KC_INTERNATIONAL_1,
+    KC_LEFT_ALT,   KC_LEFT_GUI, KC_LEFT_CTRL, KC_LEFT_ALT, LT(1,KC_LANGUAGE_2), LT(2,KC_SPACE), LT(3,KC_LANGUAGE_1), LT(2,KC_BACKSPACE), LT(1,KC_ENT), _______, _______,  _______,  KC_RALT,      QK_KB_10
   ),
 
   [1] = LAYOUT_universal(
