11f9ecb5837338f4167339e07ec9c301089dbe2e
Yama-28 <bluestrobe.48@gmail.com>
2024-06-01 21:46:49 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -23,35 +23,35 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
-    KC_ESCAPE,      KC_1,           KC_2,           KC_3,        KC_4,                KC_5,                                                     KC_6,           KC_7,        KC_8,            KC_9,          KC_0,                  KC_BSPC,
-    KC_TAB,         KC_Q,           KC_W,           KC_E,        KC_R,                KC_T,                                                     KC_Y,           KC_U,        KC_I,            KC_O,          KC_P,                  KC_LEFT_BRACKET,
-    KC_LEFT_CTRL,   KC_A,           KC_S,           KC_D,        KC_F,                KC_G,                                                     KC_H,           KC_J,        KC_K,            KC_L,          KC_SEMICOLON,          KC_QUOTE,
-    KC_LEFT_SHIFT,  KC_Z,           KC_X,           KC_C,        KC_V,                KC_B,            KC_ESCAPE,           KC_PRINT_SCREEN,    KC_N,           KC_M,        KC_COMMA,        KC_DOT,        KC_SLASH,              KC_INTERNATIONAL_1,
-    KC_LEFT_ALT,    KC_LEFT_GUI,    KC_LEFT_CTRL,   KC_LEFT_ALT, LT(1,KC_LANGUAGE_2), LT(2,KC_SPACE),  LT(3,KC_LANGUAGE_1), LT(2,KC_BACKSPACE), LT(1,KC_ENT),   _______,     _______,          _______,      KC_RALT,               QK_KB_10
+    KC_ESCAPE,      KC_1,           KC_2,           KC_3,        KC_4,                KC_5,                                                        KC_6,           KC_7,        KC_8,            KC_9,          KC_0,                  KC_BSPC,
+    KC_TAB,         KC_Q,           KC_W,           KC_E,        KC_R,                KC_T,                                                        KC_Y,           KC_U,        KC_I,            KC_O,          KC_P,                  KC_LEFT_BRACKET,
+    KC_LEFT_CTRL,   KC_A,           KC_S,           KC_D,        KC_F,                KC_G,                                                        KC_H,           KC_J,        KC_K,            KC_L,          KC_SEMICOLON,          KC_QUOTE,
+    KC_LEFT_SHIFT,  KC_Z,           KC_X,           KC_C,        KC_V,                KC_B,            LT(3,KC_ESCAPE),     LT(3,KC_PRINT_SCREEN), KC_N,           KC_M,        KC_COMMA,        KC_DOT,        KC_SLASH,              KC_INTERNATIONAL_1,
+    KC_LEFT_ALT,    KC_LEFT_GUI,    KC_LEFT_CTRL,   KC_LEFT_ALT, LT(1,KC_LANGUAGE_2), LT(2,KC_SPACE),  LT(1,KC_LANGUAGE_1), LT(2,KC_BACKSPACE),    LT(1,KC_ENT),   _______,     _______,          _______,      KC_RALT,               QK_KB_10
   ),
 
   [1] = LAYOUT_universal(
-    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,       KC_F4,               KC_F5,                                                    KC_F6,          KC_F7,       KC_F8,            KC_F9,        KC_F10,                KC_F11,
-    KC_TRANSPARENT, S(KC_1),        S(KC_2),        S(KC_3),     S(KC_4),             S(KC_5),                                                  S(KC_6),        S(KC_7),     S(KC_8),          S(KC_9),      S(KC_INTERNATIONAL_3), KC_F12,
-    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,       KC_F4,               KC_F5,                                                    S(KC_EQUAL),    KC_MINUS,    KC_RIGHT_BRACKET, KC_BACKSLASH, S(KC_SEMICOLON),       S(KC_QUOTE),
-    KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,       KC_F9,               KC_F10,          KC_F11,              KC_F12,             KC_EQUAL,       S(KC_MINUS), S(KC_COMMA),      S(KC_DOT),    S(KC_SLSH),            S(KC_INTERNATIONAL_1),
-    KC_TRANSPARENT, KC_LEFT_GUI,    KC_LEFT_CTRL,   KC_LEFT_ALT, KC_TRANSPARENT,      KC_SPACE,        KC_TRANSPARENT,      KC_BACKSPACE,       KC_ENTER,       _______,     _______,          _______,      KC_TRANSPARENT,        KC_TRANSPARENT
+    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,       KC_F4,               KC_F5,                                                       KC_F6,          KC_F7,       KC_F8,            KC_F9,        KC_F10,                KC_F11,
+    KC_TRANSPARENT, S(KC_1),        S(KC_2),        S(KC_3),     S(KC_4),             S(KC_5),                                                     S(KC_6),        S(KC_7),     S(KC_8),          S(KC_9),      S(KC_INTERNATIONAL_3), KC_F12,
+    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,       KC_F4,               KC_F5,                                                       S(KC_EQUAL),    KC_MINUS,    KC_RIGHT_BRACKET, KC_BACKSLASH, S(KC_SEMICOLON),       S(KC_QUOTE),
+    KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,       KC_F9,               KC_F10,          KC_F11,              KC_F12,                KC_EQUAL,       S(KC_MINUS), S(KC_COMMA),      S(KC_DOT),    S(KC_SLSH),            S(KC_INTERNATIONAL_1),
+    KC_TRANSPARENT, KC_LEFT_GUI,    KC_LEFT_CTRL,   KC_LEFT_ALT, KC_TRANSPARENT,      KC_SPACE,        KC_TRANSPARENT,      KC_BACKSPACE,          KC_ENTER,       _______,     _______,          _______,      KC_TRANSPARENT,        KC_TRANSPARENT
   ),
 
   [2] = LAYOUT_universal(
-    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,            KC_F5,                                                    KC_F6,          KC_F7,       KC_F8,            KC_F9,        KC_F10,               KC_F11,
-    KC_TRANSPARENT, KC_SLASH,       KC_7,           KC_8,           KC_9,             KC_MINUS,                                                 KC_NO,          KC_MS_BTN4,  KC_MS_BTN5,       KC_MS_BTN3,   KC_NO,                KC_F12,
-    KC_TRANSPARENT, S(KC_QUOTE),    KC_4,           KC_5,           KC_6,             S(KC_SEMICOLON),                                          KC_PAGE_UP,     KC_MS_BTN1,  KC_UP,            KC_MS_BTN2,   KC_HOME,              _______,
-    KC_TRANSPARENT, KC_0,           KC_1,           KC_2,           KC_3,             KC_DOT,          C(KC_Y),             C(KC_Z),            KC_PAGE_DOWN,   KC_LEFT,     KC_DOWN,          KC_RIGHT,     KC_END,               _______,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,   KC_SPACE,        KC_TRANSPARENT,      KC_DELETE,          KC_ENTER,       _______,     _______,          _______,      KC_TRANSPARENT,       QK_KB_10
+    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,            KC_F5,                                                       KC_F6,          KC_F7,       KC_F8,            KC_F9,        KC_F10,               KC_F11,
+    KC_TRANSPARENT, KC_SLASH,       KC_7,           KC_8,           KC_9,             KC_MINUS,                                                    KC_NO,          KC_MS_BTN4,  KC_MS_BTN5,       KC_MS_BTN3,   KC_NO,                KC_F12,
+    KC_TRANSPARENT, S(KC_QUOTE),    KC_4,           KC_5,           KC_6,             S(KC_SEMICOLON),                                             KC_PAGE_UP,     KC_MS_BTN1,  KC_UP,            KC_MS_BTN2,   KC_HOME,              _______,
+    KC_TRANSPARENT, KC_0,           KC_1,           KC_2,           KC_3,             KC_DOT,          C(KC_Y),             C(KC_Z),               KC_PAGE_DOWN,   KC_LEFT,     KC_DOWN,          KC_RIGHT,     KC_END,               _______,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,   KC_SPACE,        KC_TRANSPARENT,      KC_DELETE,             KC_ENTER,       _______,     _______,          _______,      KC_TRANSPARENT,       QK_KB_10
   ),
 
   [3] = LAYOUT_universal(
-    LCA(KC_ESCAPE), KC_NO,          KC_NO,          KC_NO,          KC_NO,            KC_NO,                                                    KC_NO,          KC_NO,       KC_NO,            KC_NO,        KC_NO,                KC_NO,
-    KC_TRANSPARENT, KC_NO,          KC_NO,          KC_NO,          KC_NO,            RGB_TOG,                                                  QK_KB_0,        QK_KB_1,     QK_KB_13,         QK_KB_14,     QK_KB_15,             KC_NO,
-    KC_TRANSPARENT, KC_NO,          RGB_SAI,        RGB_HUI,        RGB_VAI,          RGB_MODE_FORWARD,                                         QK_KB_4,        QK_KB_2,     QK_KB_8,          QK_KB_11,     KC_NO,                KC_NO,
-    KC_TRANSPARENT, KC_NO,          RGB_SAD,        RGB_HUD,        RGB_VAD,          RGB_MODE_REVERSE, KC_TRANSPARENT,     KC_TRANSPARENT,     QK_KB_5,        QK_KB_3,     QK_KB_9,          QK_KB_12,     KC_NO,                KC_NO,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,   KC_TRANSPARENT,   KC_TRANSPARENT,     LCA(KC_DELETE),     KC_TRANSPARENT, _______,     _______,          _______,      KC_TRANSPARENT,       LCA(KC_PAUSE)
+    LCA(KC_ESCAPE), KC_NO,          KC_NO,          KC_NO,          KC_NO,            KC_NO,                                                      KC_NO,          KC_NO,       KC_NO,            KC_NO,        KC_NO,                KC_NO,
+    KC_TRANSPARENT, KC_NO,          KC_NO,          KC_NO,          KC_NO,            RGB_TOG,                                                    QK_KB_0,        QK_KB_1,     QK_KB_13,         QK_KB_14,     QK_KB_15,             KC_NO,
+    KC_TRANSPARENT, KC_NO,          RGB_SAI,        RGB_HUI,        RGB_VAI,          RGB_MODE_FORWARD,                                           QK_KB_4,        QK_KB_2,     QK_KB_8,          QK_KB_11,     KC_NO,                KC_NO,
+    KC_TRANSPARENT, KC_NO,          RGB_SAD,        RGB_HUD,        RGB_VAD,          RGB_MODE_REVERSE, KC_TRANSPARENT,     KC_TRANSPARENT,       QK_KB_5,        QK_KB_3,     QK_KB_9,          QK_KB_12,     KC_NO,                KC_NO,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,   KC_TRANSPARENT,   KC_TRANSPARENT,     LCA(KC_DELETE),       KC_TRANSPARENT, _______,     _______,          _______,      KC_TRANSPARENT,       LCA(KC_PAUSE)
   ),
 };
 // clang-format on
@@ -66,7 +66,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     uint8_t layer = biton32(state);
     switch (layer) {
       case 0:
-        rgblight_sethsv(HSV_OFF);
+        rgblight_sethsv(HSV_WHITE);
         break;
       case 1:
         rgblight_sethsv(HSV_BLUE);
