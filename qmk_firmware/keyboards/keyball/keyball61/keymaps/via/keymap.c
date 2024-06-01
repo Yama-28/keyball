ba9005c0dbd6448127efa5852a0b2116030e6270
Yama-28 <bluestrobe.48@gmail.com>
2024-06-01 22:43:15 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -60,9 +60,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
     keyball_set_scroll_mode(get_highest_layer(state) == 3);
   
-    // LOWER + RAISE = ADJUST のようなTri Layersを使う場合
-    // これを先に書いておかないと3の色がおかしくなる
-    //state = update_tri_layer_state(state, 1, 2, 3);
     uint8_t layer = biton32(state);
     switch (layer) {
       case 0:
