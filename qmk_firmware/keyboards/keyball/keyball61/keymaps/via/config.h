7e2e70109148b62d67b24a5e81a3b4bf17845b11
Yama-28 <bluestrobe.48@gmail.com>
2024-06-01 19:08:56 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
@@ -38,6 +38,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 2
 
-#define TAPPING_TERM 180
+#define KEYBALL_SCROLLSNAP_ENABLE 1
 
 #define LAYER_LED_ENABLE
+
+#define TAPPING_TERM 180
