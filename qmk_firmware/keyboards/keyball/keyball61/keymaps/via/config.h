63e6939cb28af11e858ca745fd96a323bd48f9fa
Yama-28 <bluestrobe.48@gmail.com>
2024-06-02 22:29:16 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
@@ -49,4 +49,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PERMISSIVE_HOLD
 //#define TAPPING_FORCE_HOLD
 #define QUICK_TAP_TERM 0
-#define IGNORE_MOD_TAP_INTERRUPT
+//#define IGNORE_MOD_TAP_INTERRUPT
