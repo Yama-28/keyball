8c3fd69fcc738727f0c892d6c23907b2d7d151a5
Yama-28 <bluestrobe.48@gmail.com>
2024-06-02 22:20:48 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
@@ -44,3 +44,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define KEYBALL_SCROLL_DIV_DEFAULT 5
 
 #define TAPPING_TERM 150
+
+#define RETRO_TAPPING
+#define PERMISSIVE_HOLD
+//#define TAPPING_FORCE_HOLD
+#define QUICK_TAP_TERM 0
+#define IGNORE_MOD_TAP_INTERRUPT
