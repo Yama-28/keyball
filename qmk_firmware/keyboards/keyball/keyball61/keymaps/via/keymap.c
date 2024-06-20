cb568cae989c1bdb19ebc50bbb4c14c30ef4dd97
Yama-28 <bluestrobe.48@gmail.com>
2024-06-21 00:54:57 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [1] = LAYOUT_universal(
-    KC_TRANSPARENT, KC_1,           KC_2,           KC_3,        KC_4,                KC_5,                                                        KC_6,           KC_7,        KC_8,             KC_9,         KC_0,                  KC_BSPC,
+    KC_TRANSPARENT, KC_NO,          KC_NO,          KC_NO,       KC_NO,               KC_NO,                                                       KC_NO,          KC_NO,       KC_NO,            KC_NO,        KC_NO,                 KC_BSPC,
     KC_TRANSPARENT, S(KC_1),        S(KC_2),        S(KC_3),     S(KC_4),             S(KC_5),                                                     S(KC_6),        S(KC_7),     S(KC_8),          S(KC_9),      S(KC_INTERNATIONAL_3), S(KC_LEFT_BRACKET),
     KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,       KC_F4,               KC_F5,                                                       S(KC_EQUAL),    KC_MINUS,    KC_RIGHT_BRACKET, KC_BACKSLASH, S(KC_SEMICOLON),       S(KC_QUOTE),
     KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,       KC_F9,               KC_F10,          KC_F11,                KC_F12,              KC_EQUAL,       S(KC_MINUS), S(KC_COMMA),      S(KC_DOT),    S(KC_SLSH),            S(KC_INTERNATIONAL_1),
@@ -39,8 +39,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [2] = LAYOUT_universal(
-    KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_4,             KC_5,                                                        KC_6,           KC_7,        KC_8,             KC_9,         KC_0,                 KC_BSPC,
-    KC_TRANSPARENT, KC_SLASH,       KC_7,           KC_8,           KC_9,             KC_MINUS,                                                    KC_NO,          KC_MS_BTN4,  KC_MS_BTN5,       KC_MS_BTN3,   KC_NO,                KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_NO,          KC_NO,          KC_NO,          KC_NO,            KC_NO,                                                       KC_NO,          KC_NO,       KC_NO,            KC_NO,        KC_NO,                KC_BSPC,
+    KC_TRANSPARENT, KC_SLASH,       KC_7,           KC_8,           KC_9,             KC_MINUS,                                                    KC_NO,          KC_MS_BTN4,  KC_MS_BTN3,       KC_MS_BTN5,   KC_NO,                KC_TRANSPARENT,
     KC_TRANSPARENT, S(KC_QUOTE),    KC_4,           KC_5,           KC_6,             S(KC_SEMICOLON),                                             KC_PAGE_UP,     KC_MS_BTN1,  KC_UP,            KC_MS_BTN2,   KC_HOME,              KC_TRANSPARENT,
     KC_TRANSPARENT, KC_0,           KC_1,           KC_2,           KC_3,             KC_DOT,          C(KC_Y),               C(KC_Z),             KC_PAGE_DOWN,   KC_LEFT,     KC_DOWN,          KC_RIGHT,     KC_END,               KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,   KC_TRANSPARENT,  KC_TRANSPARENT,        KC_DELETE,           KC_ENTER,       _______,     _______,          _______,      TG(2),                KC_TRANSPARENT
@@ -66,13 +66,16 @@ layer_state_t layer_state_set_user(layer_state_t state) {
         rgblight_sethsv(HSV_OFF);
         break;
       case 1:
-        rgblight_sethsv(HSV_BLUE);
+        //rgblight_sethsv(HSV_BLUE);
+        rgblight_sethsv(240, 100, 50);
         break;
       case 2:
-        rgblight_sethsv(HSV_RED);
+        //rgblight_sethsv(HSV_RED);
+        rgblight_sethsv(0, 100, 50);
         break;
       case 3:
-        rgblight_sethsv(HSV_GREEN);
+        //rgblight_sethsv(HSV_GREEN);
+        rgblight_sethsv(120, 100, 50);
         break;
     }
   
