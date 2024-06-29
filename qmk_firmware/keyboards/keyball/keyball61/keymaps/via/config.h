f4771617f4eb78a77a1ce0ba06aff50d4670c20e
Yama-28 <bluestrobe.48@gmail.com>
2024-06-29 09:41:58 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
@@ -44,8 +44,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define KEYBALL_SCROLL_DIV_DEFAULT 5
 
 // TapからHoldに切り替わるまでの押下時間(ms)
-#undef  TAPPING_TERM
-#define TAPPING_TERM 150
+//#undef  TAPPING_TERM
+//#define TAPPING_TERM 150
 
 // TAPPING_TERMを経過しても、他のキーを押していなければTap扱い
 //#define RETRO_TAPPING
